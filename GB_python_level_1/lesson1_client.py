d19b76774e61003cda14143e5a81e256c124f6cb
Aleksandr <a.maykov@vzonedostupa.ru>
2017-11-26 00:39:06 +0300

    Update lesson1_client.py

--- GB_python_level_1/lesson1_client.py
+++ GB_python_level_1/lesson1_client.py
@@ -1,37 +1,49 @@
 import socket
+# import json
 import sys
 
 
-class Chat:
+class Chat(object):
     def __init__(self, host, port):
-        self.host = host
-        self.port = port
+        self.host = str (host)
+        self.port = int(port)
 
-    def connect(self):
-       self.sock = socket.socket()
-       self.sock.connect((self.host, self.port))
 
-    def send(self, messege):
-        self.sock.sendall(bytes(messege, 'utf-8'))
 
-        print (self.sock.recv(2048).decode())
+    # def make_messege(self, text):
+    #
+    #     self.message_text = text
+    #     self.message_object = {'message': self.message_text, 'from': 'User1'}
+    #     self.message_string = json.dumps(self.message_object)
+    #     self.message_bytes = self.message_string.encode('utf-8')
+    #     return (self.message_bytes)
 
+
+    # def send_and_print(self, messege):
+    #
+    #     self.sock.sendall(messege)
+    #     print (self.sock.recv(2048).decode())
+
+    def run(self):
+        with socket.socket() as sock:
+            self.socket = sock
+            self.socket.connect((self.host, self.port))
+
+            while True:
+                meassege = input('Введите сообщение: ')
+
+                if meassege == 'exit':
+                    self.socket.close()
+                    sys.exit()
+
+
+                sock.send(meassege.encode('utf-8'))
+                data = sock.recv(1024).decode('utf-8')
+                print(data)
 
 
 if __name__ == '__main__':
 
-    chat = Chat('localhost', 7778)
-
-    messeg = input('Привет!, пообщаемся? y\\n: ')
-
-    if messeg == 'y':
-        chat.connect()
-        while True:
-            data = chat.send (messeg)
-
-
-    else:
-        sys.exit()
-
-
+    chat = Chat('localhost', 7777)
+    chat.run()
 
