d9e998011636277abf573bbe3cbc7109174a1b29
Aleksandr <a.maykov@vzonedostupa.ru>
2017-11-27 00:36:58 +0300

    Update lesson1_client.py

--- GB_python_level_1/lesson1_client.py
+++ GB_python_level_1/lesson1_client.py
@@ -1,49 +1,50 @@
 import socket
-# import json
-import sys
 
+class Client(object):
 
-class Chat(object):
     def __init__(self, host, port):
-        self.host = str (host)
-        self.port = int(port)
 
+        self._host = str(host)
 
-
-    # def make_messege(self, text):
-    #
-    #     self.message_text = text
-    #     self.message_object = {'message': self.message_text, 'from': 'User1'}
-    #     self.message_string = json.dumps(self.message_object)
-    #     self.message_bytes = self.message_string.encode('utf-8')
-    #     return (self.message_bytes)
-
-
-    # def send_and_print(self, messege):
-    #
-    #     self.sock.sendall(messege)
-    #     print (self.sock.recv(2048).decode())
+        self._port = int(port)
 
     def run(self):
+
         with socket.socket() as sock:
+
             self.socket = sock
-            self.socket.connect((self.host, self.port))
+
+            self.socket.connect((self._host, self._port))
+            print('Приветсвую путник! Запомни exit это выход)')
 
             while True:
-                meassege = input('Введите сообщение: ')
 
-                if meassege == 'exit':
-                    self.socket.close()
-                    sys.exit()
+                msg = input('Введите сообщение: ')
 
+                if msg == 'exit':
 
-                sock.send(meassege.encode('utf-8'))
+                    break
+
+                sock.send(msg.encode('utf-8'))
+
                 data = sock.recv(1024).decode('utf-8')
-                print(data)
+
+                print('Поступил ответ: ', data)
 
 
 if __name__ == '__main__':
 
-    chat = Chat('localhost', 7777)
-    chat.run()
+    client = Client('localhost', 7777)
+
+    client.run()
+    # sock = socket.socket()
+
+    # sock.connect(('localhost', 8005))
+
+
+    # massage = sock.recv(1024)
+
+    # sock.close()
+
+    # print(massage)
 
